ffee6b406c913e32f885b577a11fee1acb159995
Anton Shliakthtin <anton.shliakhtin.dev@gmail.com>
2022-04-10 18:02:38 +0300

    - added reports to index page

--- src/main/java/global/saveukraine/volunteer/controller/MainController.java
+++ src/main/java/global/saveukraine/volunteer/controller/MainController.java
@@ -1,18 +1,37 @@
 package global.saveukraine.volunteer.controller;
 
+import global.saveukraine.volunteer.entity.Report;
+import global.saveukraine.volunteer.repo.ReportRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping
 @AllArgsConstructor
 public class MainController {
 
+  private final ReportRepository reportRepository;
+
   @GetMapping
-  public String index() {
+  public String index(Model model) {
+    List<Report> latestReports = reportRepository.findByOrderByDateTimeOfAsc()
+      .stream()
+      .limit(2)
+      .collect(Collectors.toList());
+
+    model.addAttribute("reports", latestReports);
     return "index";
+  }
+
+  @GetMapping("index")
+  public String indexPage() {
+    return "forward:/";
   }
 
   @GetMapping("about-us")
--- src/main/java/global/saveukraine/volunteer/repo/ReportRepository.java
+++ src/main/java/global/saveukraine/volunteer/repo/ReportRepository.java
@@ -4,7 +4,10 @@ import global.saveukraine.volunteer.entity.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReportRepository extends JpaRepository<Report, Long> {
 
+  List<Report> findByOrderByDateTimeOfAsc();
 }
