7f70e29c5761eda8bd22d11d0bfb2f5ecd205d3b
Anton Shliakthtin <anton.shliakhtin.dev@gmail.com>
2022-04-12 23:18:02 +0300

    - added project selection when coming to page

--- src/main/java/global/saveukraine/volunteer/controller/HelpController.java
+++ src/main/java/global/saveukraine/volunteer/controller/HelpController.java
@@ -5,10 +5,12 @@ import global.saveukraine.volunteer.service.EmailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Log4j2
 @Controller
@@ -35,7 +37,9 @@ public class HelpController {
   }
 
   @GetMapping("donation")
-  public String donation() {
+  public String donation(@RequestParam(required = false) String project,
+                         Model model) {
+    model.addAttribute("selectedProject", project);
     return "donation";
   }
 }
